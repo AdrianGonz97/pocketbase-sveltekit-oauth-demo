db73b18b248cdbeb04489dc14398078de0e5296e
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2022-11-12 16:04:26 -0500

    added oauth link

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,7 +2,10 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		pb: import('pocketbase').default;
+		user?: import('pocketbase').Record | import('pocketbase').Admin;
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
