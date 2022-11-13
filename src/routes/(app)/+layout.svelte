9aebc869c129d3e95648d5aa37cb1183c045cc03
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2022-11-13 12:31:59 -0500

    cleaned up auth

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,35 +1,16 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { PUBLIC_TWITCH_CLIENT_ID, PUBLIC_TWITCH_REDIRECT_URI } from '$env/static/public';
+	import { generateState, getAuthUrl } from '$lib/utils';
 	import type { LayoutServerData } from './$types';
 
 	export let data: LayoutServerData;
 
-	let url = new URL('https://id.twitch.tv/oauth2/authorize');
+	let url = '';
 	let state: string;
 
 	if (browser) {
-		const scopes = ['user:read:email'];
 		state = generateState();
-		url.searchParams.set('client_id', PUBLIC_TWITCH_CLIENT_ID);
-		url.searchParams.set('redirect_uri', PUBLIC_TWITCH_REDIRECT_URI);
-		url.searchParams.set('response_type', 'code');
-		url.searchParams.set('scope', scopes.join('+'));
-		url.searchParams.set('state', state);
-	}
-
-	function generateState() {
-		const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-		let array = new Uint8Array(43);
-
-		window.crypto.getRandomValues(array);
-		array = array.map((x) => validChars.charCodeAt(x % validChars.length));
-
-		// @ts-ignore
-		const randomState = String.fromCharCode.apply(null, array);
-
-		return randomState;
+		url = getAuthUrl(state).toString();
 	}
 </script>
 
@@ -42,7 +23,7 @@
 			</form>
 		{:else}
 			<a
-				href={url.toString()}
+				href={url}
 				on:click={() => {
 					window.localStorage.setItem('state', state);
 				}}>Connect with Twitch</a
