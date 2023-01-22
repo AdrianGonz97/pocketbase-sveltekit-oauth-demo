6105e9fbd47ae070ecabb269182af2e05062679b
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-01-22 11:53:00 -0500

    don't perform auth check in layout

--- src/routes/(app)/(protected)/profile/+page.ts
+++ src/routes/(app)/(protected)/profile/+page.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
-import type { LayoutLoad } from './$types';
+import type { PageLoad } from './$types';
 
-export const load: LayoutLoad = async ({ parent }) => {
+export const load: PageLoad = async ({ parent }) => {
 	const { user } = await parent();
 
 	if (!user) throw error(401, 'Unauthorized');
